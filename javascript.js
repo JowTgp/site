361cc4096ca254b9f6ec9f8f1c772fee068352f5
Jolina <jolina.arryn@student.ucll.be>
2025-03-28 15:10:56 +0100

    test menu

--- javascript.js
+++ javascript.js
@@ -1,8 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Script geladen!"); // Controleert of het script werkt
+
+
+
 const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
 const body = document.body;
+
+
+if (!hamMenu || !offScreenMenu) {
+  console.error("Elementen niet gevonden!"); // Check of de elementen bestaan
+  return;
+}
 
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
