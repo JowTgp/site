9eb8204b9ed4866e80ef2c32d858adb8c8e85fac
Jolina <jolina.arryn@student.ucll.be>
2025-03-28 14:54:09 +0100

    Toevoegingen 28/3

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
+const body = document.body;
+
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
